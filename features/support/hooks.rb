d6a8818c57d9c5b9a0b8fb388a69935e8cfdab20
ericayala <emast3@mail.rmu.edu>
2020-08-06 23:24:09 -0400

    Added a comment in regards to re-sizing the Chrome window.

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -6,9 +6,11 @@ Before do
   options = Selenium::WebDriver::Chrome::Options.new
   options.add_argument('disable-dev-shm-usage')
   options.add_argument('no-sandbox')
-  @browser = Watir::Browser.new :chrome, :options => options
+  @browser = Watir::Browser.new :chrome, options: options
   screen_width = @browser.execute_script('return screen.width;')
   screen_height = @browser.execute_script('return screen.height;')
+  # Returns the size of the current resolution and re-sizes the Chrome window
+  # to it in order to run the test in full screen.
   @browser.driver.manage.window.resize_to(screen_width, screen_height)
 
   # This will set the default locale of Faker to the United States, using English.
